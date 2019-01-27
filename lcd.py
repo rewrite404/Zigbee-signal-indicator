0966b2365b9557f2fd7433b0bf012b4271417423
rewrite404 <aaronlin1996@gmail.com>
2019-01-27 15:46:54 +0800

    fix bug
    added sampleing mesurement
    fix txpowmode parameter

--- lcd.py
+++ lcd.py
@@ -17,11 +17,11 @@ class Lcd:
         self.label_font = ImageFont.truetype('HelveticaBlkIt.ttf', 12)
 
         self.display.begin()
-
+        self.signal_state = ''
         # Load an image.
         image = Image.open('logo.jpg').rotate(0).resize((self.WIDTH, self.HEIGHT))
         self.display.display(image)
-        # self.draw = self.display.draw()
+        self.draw = self.display.draw()
 
     @staticmethod
     def draw_rotated_text(image, text, position, angle, font, fill=(255, 255, 255)):
@@ -38,9 +38,26 @@ class Lcd:
         # Paste the text into the image, using it as a mask for transparency.
         image.paste(rotated, position, rotated)
 
-    def redraw(self, channel, state='N/A', lqi='N/A', rssi='N/A'):
+    def redraw(self, channel, state='N/A', lqi='N/A', rssi='N/A',):
+        if state == 'Receive':
+            return
         self.display.clear()
         self.draw_rotated_text(self.display.buffer, state, (10, 10), 0, self.font1, fill=(0, 0, 255))
+        if state == 'Poor':
+            self.draw.rectangle((8, 40, 50, 50), outline=(0, 0, 255), fill=(0, 0, 255))
+        elif state == 'Bad':
+            self.draw.rectangle((8, 40, 50, 50), outline=(0, 0, 255), fill=(0, 0, 255))
+            self.draw.rectangle((50, 40, 70, 50), outline=(0, 110, 255), fill=(0, 110, 255))
+        elif state == 'Good':
+            self.draw.rectangle((8, 40, 50, 50), outline=(0, 0, 255), fill=(0, 0, 255))
+            self.draw.rectangle((50, 40, 70, 50), outline=(0, 110, 255), fill=(0, 110, 255))
+            self.draw.rectangle((70, 40, 90, 50), outline=(0, 245, 255), fill=(0, 245, 255))
+        else:
+            self.draw.rectangle((8, 40, 50, 50), outline=(0, 0, 255), fill=(0, 0, 255))
+            self.draw.rectangle((50, 40, 70, 50), outline=(0, 110, 255), fill=(0, 110, 255))
+            self.draw.rectangle((70, 40, 90, 50), outline=(0, 245, 255), fill=(0, 245, 255))
+            self.draw.rectangle((90, 40, 118, 50), outline=(0, 255, 0), fill=(0, 255, 0))
+        self.signal_state = state
         self.draw_rotated_text(self.display.buffer, 'CHANNEL', (10, 100), 0, self.label_font, fill=(0, 255, 0))
         self.draw_rotated_text(self.display.buffer, str(channel), (75, 90), 0, self.font1, fill=(255, 255, 255))
         self.draw_rotated_text(self.display.buffer, 'LQI', (10, 120), 0, self.label_font, fill=(0, 255, 0))
@@ -48,4 +65,3 @@ class Lcd:
         self.draw_rotated_text(self.display.buffer, 'RSSI', (70, 120), 0, self.label_font, fill=(0, 255, 0))
         self.draw_rotated_text(self.display.buffer, rssi, (70, 130), 0, self.font1, fill=(255, 255, 255))
         self.display.display()
-
